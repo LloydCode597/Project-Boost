db6016d421c39861d9bb99f2ff17f6016d46f102
Lloyd Allen <lloydallen597@gmail.com>
2024-02-27 22:11:57 -0600

    Working on player movement and thruster

--- Project Boost/Assets/Scripts/Movement.cs
+++ Project Boost/Assets/Scripts/Movement.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    [SerializeField] float mainThrust = 100f;
+    [SerializeField] float rotationThrust = 1f;
+
     Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-         rb = GetComponent<Rigidbody>();
+          rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -24,21 +28,31 @@ public class Movement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            Debug.Log("Pressed SPACE - Thrusting");
-            rb.AddRelativeForce(Vector3.up);
+            // Debug.Log("Pressed SPACE - Thrusting");
+            rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
         }
     }
 
      void ProcessRotation()
     {
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
             Debug.Log("Rotating Left");
+            ApplyRotation(rotationThrust);
         }
-        else if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             Debug.Log("Rotating Right");
+            ApplyRotation(-rotationThrust);
         }
     }
+
+    void ApplyRotation(float rotationThisFrame)
+    {
+        rb.freezeRotation = true;  // freezing rotation so we can manually rotate
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
+        rb.freezeRotation = false;  // unfreezing rotation so the physics system can take over
+    }
+
 
 }
