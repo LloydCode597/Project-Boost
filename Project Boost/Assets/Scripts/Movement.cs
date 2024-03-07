4225f31d09983e3d4bf366a4bbf7eb82e7802165
Lloyd Allen <lloydallen597@gmail.com>
2024-03-06 23:16:13 -0600

    Implemented audio for the rocket

--- Project Boost/Assets/Scripts/Movement.cs
+++ Project Boost/Assets/Scripts/Movement.cs
@@ -7,13 +7,14 @@ public class Movement : MonoBehaviour
 {
     [SerializeField] float mainThrust = 100f;
     [SerializeField] float rotationThrust = 1f;
-
     Rigidbody rb;
+    AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
-          rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -30,6 +31,15 @@ public class Movement : MonoBehaviour
         {
             // Debug.Log("Pressed SPACE - Thrusting");
             rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+            if(!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
+
         }
     }
 
