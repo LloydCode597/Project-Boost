e105551a13c95a2bccb592770165b588b095d3fc
Lloyd Allen <lloydallen597@gmail.com>
2024-03-12 00:04:54 -0500

    Able to load the next Level

--- Project Boost/Assets/Scripts/CollisionHandler.cs
+++ Project Boost/Assets/Scripts/CollisionHandler.cs
@@ -13,7 +13,7 @@ public class CollisionHandler : MonoBehaviour
                 Debug.Log("This thing is friendly");
                 break;
             case "Finish":
-                Debug.Log("Congrats, yo, you finished!");
+                LoadNextLevel();
                 break;
             case "Fuel":
                 Debug.Log("You picked up fuel");
@@ -23,6 +23,18 @@ public class CollisionHandler : MonoBehaviour
                 break;
         }
     }
+void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+
+    }
+
     void ReloadLevel()
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
